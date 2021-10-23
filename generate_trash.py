a66c2d67d4acdb5ceead760bd43219161f2b3614
István Mayer <adeptus19@gmail.com>
2021-10-23 10:39:35 +0200

    First working version, needs write to file

--- generate_trash.py
+++ generate_trash.py
@@ -23,31 +23,34 @@ sqlhandle = open(sqlfile, "w")
 read_lines = 0;
 s1 = "2021/01"
 index = 1
-sqlcommand = "INSERT INTO selejtj (sorszam, jegyzek, ar, pnem, szerzo, fc, szakj, raktj, lbetu, lszam, lhely, kotszam, db, torles, doktipus, kpld) VALUES ("
+sqlcommand = "INSERT INTO selejtj (sorszam, jegyzek, ar, lszam, szerzo, cim, szakj, raktj, pnem, lbetu, lhely, kotszam, db, torles, doktipus, kpld) VALUES ("
 for id in fhandle:
     id = id.rstrip()
     values = ""
     read_lines = read_lines + 1
-    search  = "SELECT konyv.id, ar, konyv.doktipus,kpld.sorszam,szerzo,fc,szakj,raktj,pnem,lbetu,lszam,lhely,kotszam FROM konyv INNER JOIN kpld ON konyv.id = kpld.id WHERE vonalkod = '" + id +" ';"
+    search  = "SELECT kpld.sorszam, konyv.id, ar, lszam, szerzo,fc,szakj,raktj,pnem,lbetu, lhely,kotszam FROM konyv INNER JOIN kpld ON konyv.id = kpld.id WHERE vonalkod = '" + id +" ';"
     dbc.execute(search)
     res=dbc.fetchone()
     if (not (res is None)):
         values = str(index) +", '" + s1 + "', "
         print(read_lines)
-        if (float(res[1])> 0):
-            values = values + str(res[1]) + ", "
+        if (float(res[2])> 0):
+            values = values + str(res[2]) + ", "
         else:
             values = values + "0, "
-        for i in range(2,len(res) - 1 ):
+        values = values + str(res[3]) + ", "
+        for i in range(4,len(res) - 1 ):
             if (res[i] != ""):
                 values = values + "'" + str(res[i]) + "', "
             else:
                 values = values + "' ', "
-        print(values)
-        if ((res[12] != "") and (res[12] is not None)):
-            values = values + "'" + str(res[13]) + "', "
+        if ((res[11] != "") and (res[11] is not None)):
+            values = values + "'" + str(res[11]) + "', "
         else:
             values = values + "' ', "
+        values = values + "1, " + "'Elavulás', " + "'Könyv', " + str(res[0]) +");"
+        list = sqlcommand + values 
+        print(list)
         index = index + 1
 fhandle.close
 sqlhandle.close()
